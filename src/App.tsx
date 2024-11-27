8511d41fad335e2046a0dff6dab2a1e6e3c6b883
heath <jannhyunsoo@gmail.com>
2024-11-27 23:24:21 +0900

    settings >redux,redux-saga

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { SnackbarProvider } from 'notistack';
 
 import { Header, Body, Footer, Wrapper } from './App.styled';
 import { theme } from "./styles/theme";
+import { Provider } from "react-redux";
+import { store, persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 
 const App: React.FC = () => {
@@ -13,17 +16,22 @@ const App: React.FC = () => {
         <SnackbarProvider maxSnack={3}>
             <BrowserRouter>
                 <ThemeProvider theme={theme}>
-                    <Wrapper>
-                        <Header>
-                            Header Section
-                        </Header>
-                        <Body>
-                            This is the body content.
-                        </Body>
-                        <Footer>
-                            Footer Section
-                        </Footer>
-                    </Wrapper>
+                    <Provider store={store}>
+                        <PersistGate loading={null} persistor={persistor}>
+
+                            <Wrapper>
+                                <Header>
+                                    Header Section
+                                </Header>
+                                <Body>
+                                    This is the body content.
+                                </Body>
+                                <Footer>
+                                    Footer Section
+                                </Footer>
+                            </Wrapper>
+                        </PersistGate>
+                    </Provider>
                 </ThemeProvider>
             </BrowserRouter>
         </SnackbarProvider>
